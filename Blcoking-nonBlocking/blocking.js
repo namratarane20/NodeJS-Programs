25fa5ab8160ba4c8ff73a77d65e5af83b37d79f2
Namrata Ashok <namrata.ashok@impelsys.com>
2020-03-24 11:16:56 +0530

    node js program for blocking and non blocking examples

---
+++ Blcoking-nonBlocking/blocking.js
@@ -0,0 +1,6 @@
+const getAddSub = require('./util.js')
+const addition = getAddSub(10,20)
+
+console.log(addition)
+ const mul =  70 *10
+ console.log(mul)
---
+++ Blcoking-nonBlocking/util.js
@@ -0,0 +1,16 @@
+// console.log('I  am from utils.js')
+// //  const name = 'namrata'
+// //  module.exports = name
+// const  add = function(a, b){
+//     return  a+b
+// }
+//  module.exports = add
+console.log('O am from util file')
+const add = function(a,b){
+    console.log('addition is-------',a+b)
+    console.log('substarction is------', a-b)
+}
+// const sub = function(x,y){
+//    return x-y
+// }
+ module.exports =add
